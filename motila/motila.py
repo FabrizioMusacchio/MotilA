f5d0d769c23b17a4ec6aac3097356209d7c8c744
Fabrizio Musacchio <fabrizio.musacchio@posteo.de>
2025-03-27 10:07:33 +0100

    Remove unnecessary blank line in batch_process_stacks function

--- motila/motila.py
+++ motila/motila.py
@@ -2820,7 +2820,6 @@ def batch_process_stacks(PROJECT_Path, ID_list=[], project_tag="TP000", reg_tif_
     if debug_output: print_ram_usage()
 
 
-
 def batch_collect(PROJECT_Path, ID_list=[], project_tag="TP000", motility_folder="motility_analysis",
                   RESULTS_Path="batch_results", log=""):
     """
