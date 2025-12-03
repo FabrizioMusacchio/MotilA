d5bce9de57d035a1eacb4f2b775b66eaeef499ac
Fabrizio Musacchio <fabrizio.musacchio@posteo.de>
2025-12-03 10:40:49 +0100

    Refactor imports in __init__.py: Added a few more utils functions to the public API

--- motila/__init__.py
+++ motila/__init__.py
@@ -4,14 +4,28 @@ from .motila import (
     batch_process_stacks,
     batch_collect,
 )
-from .utils import tiff_axes_check_and_correct
+from .utils import (
+    tiff_axes_check_and_correct,
+    check_folder_exist_create,
+    filterfolder_by_string,
+    filterfiles_by_string,
+    logger_object,
+    print_ram_usage_in_loop,
+    print_ram_usage
+)
 
 __all__ = [
     "hello_world",
     "process_stack",
     "batch_process_stacks",
     "batch_collect",
-    "tiff_axes_check_and_correct"
+    "tiff_axes_check_and_correct",
+    "check_folder_exist_create",
+    "filterfolder_by_string",
+    "filterfiles_by_string",
+    "logger_object",
+    "print_ram_usage_in_loop",
+    "print_ram_usage",
 ]
 
 # expose the motila submodule for backward compatibility
